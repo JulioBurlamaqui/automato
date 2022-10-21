02f3de8a2eba50f963f3c19974768b2a648c1280
Júlio Burlamaqui <64053910+JulioBurlamaqui@users.noreply.github.com>
2022-10-20 23:18:56 -0300

    Corrigindo tipo do vetor
    Mudando Vector<Charset> para Vector<Character>

--- Automato.java
+++ Automato.java
@@ -1,4 +1,4 @@
-import java.nio.charset.Charset;
+import java.nio.Character.Character;
 import java.util.*;
 
 public class Automato
@@ -11,12 +11,12 @@ public class Automato
 	 * dicionário possui um par estado entrada-caractere como chave e um estado resultado como valor.
 	 */
     private int estadoInicial;
-    private Vector<Charset> alfabeto;
+    private Vector<Character> alfabeto;
     private Vector<Integer> estados;
     private Vector<Integer> estadosFinais;
     private Dictionary<Par, Integer> trans;
 
-    Automato(Vector<Charset> alfabeto, Vector<Integer> estadosFinais, Vector<Integer> estados, Dictionary<Par, Integer> trans)
+    Automato(Vector<Character> alfabeto, Vector<Integer> estadosFinais, Vector<Integer> estados, Dictionary<Par, Integer> trans)
     {
         this.estadoInicial = 1; /*estado inicial é sempre zero kk*/
         this.alfabeto = alfabeto;
@@ -34,14 +34,14 @@ public class Automato
     {
     	Scanner sc = new Scanner(System.in);
     	char simbolo;
-    	String lexema = "";
+    	Vector<Character> lexema = new Vector<Character>();
     	int estadoAtual = estadoInicial;
     	Par entrada;
     	
     	while(sc.hasNext())
     	{
     		simbolo = sc.next().charAt(0);
-    		lexema += simbolo;
+    		lexema.add(simbolo);
     		entrada = new Par(estadoAtual, simbolo);
     		
     		estadoAtual = funcao_trans(entrada);
